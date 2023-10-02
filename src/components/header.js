1c6c6159023e3d41739e47f95499c124aebd1bbf
Temiloluwaakin <106915700+Temiloluwaakin@users.noreply.github.com>
2023-10-02 09:48:14 +0100

    second commit

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,14 @@ import { Link } from "react-router-dom";
 import "./Styles.css"
 import logo from '../images/Logo.png';
 
+import { slideToggle } from "./slideToggle/index.js"
+import { useRef } from "react";
+
 const Header = () => {
+
+    const dropdowncaret = useRef(null);
+
+
     return (
         <div className="header">
             <div className="left">
@@ -14,6 +21,15 @@ const Header = () => {
                 <Link to='#'>Blog</Link>
                 <Link to='#'>Contact Us</Link>
                 <button>Get Started</button>
+            </div>
+            <div className='dropdowncaret' onClick={ () => slideToggle(dropdowncaret.current)}>
+                <span style={ { fontSize: "20px", paddingBottom: "0px", marginLeft: "20px" } }><span className="drpd">menu</span></span>
+                <div ref={dropdowncaret} className="target-div thedropdown" style={{ display: "none"}}>
+                    <Link to="/" className='nav-link'> Products </Link>
+                    <Link to="/about" className='nav-link'> About Us </Link>
+                    <Link to="/projects" className='nav-link'> Blogs </Link>
+                    <Link to="/contact" className='nav-link'> Contact Us </Link>
+                </div>
             </div>
         </div>
     );
